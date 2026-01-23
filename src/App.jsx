b0b58a11254ccaff0f70df436b38d99feb65555d
saurabh-77777 <153528949+saurabh-77777@users.noreply.github.com>
2026-01-23 11:48:56 +0530

    Change import path for Login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import Login from "./components/About.jsx";
+import Login from "./components/login.jsx";
 import Contact from "./components/Contact.jsx";
 
 export default function App() {
