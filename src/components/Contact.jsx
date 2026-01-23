fed300f27a92972a45a578779d9af3d1c45a7c33
saurabh-77777 <153528949+saurabh-77777@users.noreply.github.com>
2026-01-23 12:00:17 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,8 @@ export default function Contact() {
   return (
     <section id="contact" className="py-20 bg-gray-600 text-white text-center">
       <h2 className="text-4xl font-bold">Contact Me</h2>
-      <p className="mt-4">100rabh@gmail.com</p>
+      <p className="mt-4">Gmail : 100rabh@gmail.com</p>
+      <p className="mt-4">Phone No. : 8853465878</p>
     </section>
   );
 }
